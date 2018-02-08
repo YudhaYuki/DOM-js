e58519e0c0446d7aec4508b3b185aff4d2e4b5e3
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-08 17:33:29 +0100

    Adding text input for final score, USER CAN INPUT WINNING SCORE by themselve instead of must always reaching 100 to win

--- challanges.js
+++ challanges.js
@@ -58,9 +58,21 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     
         // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-    
+
+        // VALUE; this give us the content that the user put into this inut field
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+        // Undefined, 0, null or "" are COERCED to false
+        // Anything else is COERCED to true
+        if(input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+
         // Check if the player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
