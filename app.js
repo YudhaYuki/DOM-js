7e2beca064463f24ce5d4f86029de6592135b90c
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-08 11:56:14 +0100

    Setting the active player to 0, when we START the game

--- app.js
+++ app.js
@@ -136,6 +136,17 @@ function init() {
     document.getElementById('current-1').textContent = '0';
     document.getElementById('name-0').textContent = 'Player 1!';
     document.getElementById('name-1').textContent = 'Player 2!';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    // Setting the active player to 0, when we START the game
+    document.querySelector('.player-0-panel').classList.add('active');
+    
+
+    
+    
     
     
 };
